c5d69f4d7af29c98383c00d5d94d251f9507da68
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-20 08:31:36 -0700

    Port preferences unicode parse error

---
+++ src/commonMain/kotlin/io/github/kotlinmania/iculocalecore/preferences/extensions/unicode/Errors.kt
@@ -0,0 +1,20 @@
+// port-lint: source preferences/extensions/unicode/errors.rs
+package io.github.kotlinmania.iculocalecore.preferences.extensions.unicode
+
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+// Errors related to parsing preferences.
+
+/**
+ * Error returned by parsers of Unicode extensions as preferences.
+ *
+ * This enum is non-exhaustive: new variants may be added in future releases.
+ */
+enum class PreferencesParseError {
+    /**
+     * The given keyword value is not a valid preference variant.
+     */
+    InvalidKeywordValue,
+}
